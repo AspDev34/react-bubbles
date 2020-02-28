9254339448254dd1d7fd5391cf0057de2242e2fd
Ike Otu <ikeotu@msn.com>
2020-02-28 09:23:56 -0800

    initial commit, Private Route added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './components/PrivateRoute';
 import Login from "./components/Login";
+
 import "./styles.scss";
 
 function App() {
@@ -13,6 +16,8 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute path='/protected' component={BubblePage}/>
+          
       </div>
     </Router>
   );
